2eda9cb0fa942d1eb3c9d1be23eb1a3894254e91
martonTorok <tormartonz@gmail.com>
2018-09-07 15:01:16 +0200

    changed idCounter to postfix

--- src/main/java/application/model/Account.java
+++ src/main/java/application/model/Account.java
@@ -1,5 +1,7 @@
 package model;
 
+import sun.util.locale.provider.AvailableLanguageTags;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +12,14 @@ public class Account {
     private static int idCounter = 0;
     private BigDecimal balance;
     private List<Transaction> transactionHistory;
+    public static List<Account> bankAccounts = new ArrayList<>();
 
     public Account(User user) {
         this.user = user;
-        this.id = ++idCounter;
+        this.id = idCounter++;
         this.balance = new BigDecimal(0.0000);
         this.transactionHistory = new ArrayList();
+        bankAccounts.add(this);
     }
 
     public List getTransactionHistory() {
@@ -42,4 +46,7 @@ public class Account {
         return balance;
     }
 
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
