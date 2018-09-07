6f7bc4c2ee76512440d71e6e20ec9c289676e5f1
martonTorok <tormartonz@gmail.com>
2018-09-07 14:04:18 +0200

    transactionHistory field added to account model

--- src/main/java/application/model/Account.java
+++ src/main/java/application/model/Account.java
@@ -1,17 +1,25 @@
 package model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Account {
     private User user;
     private int id;
     private static int idCounter = 0;
     private BigDecimal balance;
+    private List transactionHistory;
 
     public Account(User user) {
         this.user = user;
         this.id = ++idCounter;
         this.balance = new BigDecimal(0.0000);
+        this.transactionHistory = new ArrayList();
+    }
+
+    public List getTransactionHistory() {
+        return transactionHistory;
     }
 
     public User getUser() {
