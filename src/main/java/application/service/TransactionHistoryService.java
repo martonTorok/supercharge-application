f25a0b2351f6b1fe3dfc2519bd4e476f1d800b60
martonTorok <tormartonz@gmail.com>
2018-09-07 15:34:03 +0200

    implemented transactionhistory filtering method

--- src/main/java/application/service/TransactionHistoryService.java
+++ src/main/java/application/service/TransactionHistoryService.java
@@ -2,8 +2,10 @@ package service;
 
 import model.Account;
 import model.Transaction;
+import model.TransactionType;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TransactionHistoryService {
@@ -13,11 +15,15 @@ public class TransactionHistoryService {
         transactions.add(transaction);
     }
 
-    public List<Transaction> getTransactionsByAccountId(int accountId) {
+    public List<Transaction> getTransactionsByAccountIdFilterByTypeAndDate(int accountId, TransactionType type, Date date) {
         List<Transaction> transactionList = new ArrayList<>();
         for(Transaction transaction : transactions) {
             if (transaction.getSourceAccountId() == accountId || transaction.getTargetAccountId() == accountId) {
-                transactionList.add(transaction);
+                if (transaction.getType() == type) {
+                    if (transaction.getDateTime().compareTo(date) > 0) {
+                        transactionList.add(transaction);
+                    }
+                }
             }
         }
         return transactionList;
