be2d8d47df1ffdd474d464eebae3786bc6cb9a3a
martonTorok <tormartonz@gmail.com>
2018-09-07 14:00:00 +0200

    Basic transaction + transactiontype model added

---
+++ src/main/java/application/model/Transaction.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Transaction {
+    private static int idCounter = 0;
+    private int transactionId;
+    private TransactionType type;
+    private BigDecimal amountOfMoney;
+    private int sourceAccountId;
+    private int targetAccountId;
+    private Date dateTime;
+
+    public Transaction(TransactionType transactionType, BigDecimal amountOfMoney, int sourceAccountId, int targetAccountId) {
+        this.type = transactionType;
+        this.amountOfMoney = amountOfMoney;
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.transactionId = ++idCounter;
+        this.dateTime = new Date();
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public static int getIdCounter() {
+        return idCounter;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public BigDecimal getAmountOfMoney() {
+        return amountOfMoney;
+    }
+
+    public int getSourceAccountId() {
+        return sourceAccountId;
+    }
+
+    public int getTargetAccountId() {
+        return targetAccountId;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+}
